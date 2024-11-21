4134832a9ffd349e0c506aafe4117371559fb2f8
IsmailYucelOlmez <ismailyucelolmez514@gmail.com>
2024-11-21 12:29:45 +0300

    Page and Component Files Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import GamesPage from './pages/GamesPage'
+import GameDetailsPage from './pages/GameDetailsPage'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div>
-      <h1>ÖğreniLand</h1>
+      <Header/>
+
+      <Routes>
+        <Route path='/' element={<HomePage/>} />  
+        <Route path='/about' element={<AboutPage/>} />
+        <Route path='/games' element={<GamesPage/>} />
+        <Route path='/game/:id' element={<GameDetailsPage/>} />
+      </Routes>
+
+      <Footer/>
     </div>
   )
 }
